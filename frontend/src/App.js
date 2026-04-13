35e478ee056841d5e4ec2b860ee15518f08b3273
Luckyy27 <d.lakshman2004@gmail.com>
2026-04-13 18:29:02 +0530

    fixed duplicates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+//import { Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from "react";
+//import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
